4c7ab2a7aa69aea2af54fe3901eb8aad06e3a461
lzxxm <1052334039@qq.com>
2019-05-30 13:45:10 +0800

    ..

--- vps.sh
+++ vps.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-git clone https://github.com/birdsofsummer/shop.git /tmp/shop
+git clone -b dist https://github.com/birdsofsummer/shop.git /tmp/shop
 cp -R /tmp/shop/static /opt/jiakai/
 rm -rf /tmp/shop
